562780ac235a1c29edc48929b1e562ec91501f59
nandju <nlbmd17@gmail.com>
2025-08-19 11:26:55 +0000

    préparatif presque achevez, reste les images, les vidéos et le logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,12 @@
 import Header from "@/components/header"
-import MayorMessage from "@/components/mayor-message/mayor-message"
-import { Testimonials } from "@/components/testimonials/testimonials"
-import Services from "@/components/services/services"
-import News from "@/components/news/news"
-import HeroSection from "@/components/hero/hero-section"
+
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
       {/* <Header /> */}
-      <HeroSection />
-      <MayorMessage />
-      <Services />
-      <News />
-      <Testimonials />
+
     </main>
   )
 }
